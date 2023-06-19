d1a998219d7bc97b576a6876f9752891557b21a7
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-19 13:02:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,15 +6,17 @@
 // module.exports = palindrome
 
 
-function isPalindrome(s) {
+
+function Palindrome(str) {
   // Remove non-alphanumeric characters and convert to lowercase
-  s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+ // s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
   // Check if the string is equal to its reverse
   return s === s.split('').reverse().join('');
 }
 
 // Example usage:
-const input = "racecar";
-const result = isPalindrome(input);
+const input = "race a car";
+const result = Palindrome(input);
 console.log(result);
+
