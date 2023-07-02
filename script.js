5e760eaefa171969a1e26065bd160ed2545e7654
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-07-02 21:36:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function isPalindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+  // Reverse the cleaned string
+  const reversedStr = cleanStr.split('').reverse().join('');
+  
+  // Check if the original and reversed strings are equal
+  return cleanStr === reversedStr;
+}
+
+
+
 // // complete the given function
 
 // function palindrome(str){
@@ -7,13 +20,13 @@
 
 
 
-function Palindrome(str) {
-  // Remove non-alphanumeric characters and convert to lowercase
- // s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+// function Palindrome(str) {
+//   // Remove non-alphanumeric characters and convert to lowercase
+//  // s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
-  // Check if the string is equal to its reverse
-  return str === str.split('').reverse().join('');
-}
+//   // Check if the string is equal to its reverse
+//   return str === str.split('').reverse().join('');
+// }
 
 // Example usage:
 const input = "race a car";
